ff3a0d00b97ba3d18683848282369fe9aae4cd53
ehdrnjs9022 <ehdrnjs9022@naver.com>
2025-01-20 11:25:43 +0900

    java workspace 등록

---
+++ 00_JavaTest/src/Test1.java
@@ -0,0 +1,21 @@
+// JS와 주석 동일
+
+/*Run 버튼 (단축키 ctrl + f11)
+ * 1) 자동으로 컴파일 수행(javac ooo.java) -> .class 파일 생성
+ *   -> .class 파일(byte code) 생성
+ *   
+ * 2) 컴파일 완료 후 .class 파일 실행 (java class파일명)
+ * */
+
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("이클립스 실행 중");
+		System.out.println("월요일 싫다");
+		
+	}
+
+}
