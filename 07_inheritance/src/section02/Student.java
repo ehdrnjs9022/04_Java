1134ac2782872d1980e7d72daa71bafa80420829
ehdrnjs9022 <ehdrnjs9022@naver.com>
2025-02-04 16:46:56 +0900

    20250204다형성

--- 07_inheritance/src/section02/Student.java
+++ 07_inheritance/src/section02/Student.java
@@ -10,8 +10,8 @@ public class Student extends Person{
 	
 	/* super() 생성자 
 	 * - super : 극상의, 상위의, 위쪽의
-	 * 
-	 * - 현재 클래스의 상위 클래스(부모) 생성자를 호출하는 구문
+	 *	- 자식 클래스에서 상위(부모) 클래스 생성자를 호출하는 구문   
+	 * 	- 현재 클래스의 상위 클래스(부모) 생성자를 호출하는 구문
 	 * 
 	 * - super() 생성자는 반드시
 	 *  자식 생성자의 첫 번째 줄에 작성 되어야 한다!!!
@@ -20,10 +20,9 @@ public class Student extends Person{
 	 *  - 코드 길이 감소, 재사용성 증가
 	 */
 	
-	
 	// 기본 생성자
 	public Student() {
-		super();
+		super(); // 부모 기본 생성자
 		System.out.println("[Student 기본 생성자로 생성됨]");
 	}
 	
@@ -41,7 +40,6 @@ public class Student extends Person{
 		
 		this.schoolName=schoolName;
 		
-		
 	}
 	// getter/setter
 	public String getSchoolName() {
@@ -52,9 +50,47 @@ public class Student extends Person{
 		this.schoolName = schoolName;
 	}
 	
+	/* super 참조 변수(부모 참조 변수)
+	 * - 자식 내 부모 객체를 참조하는 변수
+	 * - 부모의 필드/메서드에 접근하기 위해 사용 
+	 * 
+	 */
+	
+	// 연속으로 재정의
+	// Object.toString() -> Pereson.toString() -> Student.toString(0
+	@Override
 	public String toString() {
-		return String.format("name : %s / age : %d / schoolName : %s",
-													getName(), getAge(), schoolName);
+		
+		// super.toString() : 부모의 toString() 메서드 호출
+		// -> name : 홍길동 / age 
+		return super.toString() + "/ schoolName : " + schoolName;
+	}
+	
+	
+//	public String toString() {
+//		return String.format("name : %s / age : %d / schoolName : %s",
+//													getName(), getAge(), schoolName);
+//	}
+	
+	
+	/* Person.introduce() 메서드 오버라이딩(재정의) */
+	
+	/* 오버라이딩 성립 조건
+	 * [변경 불가]
+	 * - 반환형
+	 * - 메서드명
+	 * - 매개변수(개수, 순서, 타입)
+	 * 
+	 * [변경 가능]
+	 * - 접근 제어자(기존 -> 넓은 범위)
+	 * - 예외처리   (기존 -> 좁은 범위)
+	 * 
+	 */
+	
+	@Override // 오버라이딩 명시 + 성립 조건 맞는지 검사 지시
+	public String introduce(String alias) {
+		return String.format("[Student가 재정의] 내 이름은 %s, %s 이죠!",
+													super.getName() ,alias);
 		
 	}
 	
---
+++ 08_polymorphism/src/section01/service/PolymorphismService.java
@@ -0,0 +1,259 @@
+package section01.service;
+
+import section01.dto.Parent;
+import section01.dto.Child;
+
+public class PolymorphismService {
+	
+	/* [매개 변수의 다형성]
+	 * 
+	 * - 매개 변수의 타입을 "부모 타입"으로 설정하여 
+	 * 모든 자식 객체 참조 주소를 전달 받을 수 있도록 함 
+	 * (다형성 - 업캐스팅 적용)
+	 * 
+	 * * 매개 변수(Parameter) : 메서드 호출 시 전달된 값을 저장하는 변수
+	 */
+	
+	/* 객체 지향 프로그래밍
+	 * - 현실은 객체와 객체의 상호 작용으로
+	 * 사건이 발생한다는 개념을 컴퓨터로 옮겨놓은 것
+	 *
+	 * - 객체는 속성/기능으로 이루어짐
+	 * 
+	 * class : 객체의 속성과 기능을 정의한 것(문서, 설계도)
+	 * 
+	 * instance : class에 작성된 내용을 토대로 메모리에 생성된 객체
+	 */
+
+	/* * 참조변수 instanceof 클래스명
+	 * 
+	 * - "참조변수"가 참조하는 객체가
+	 *  "클래스명"로 만들어진 객체가 맞을 경우 true, 아님 false
+	 *  
+	 *  - 상속 관계도 파악이 가능
+	 *  	(자식 객체 내부의 부모 객체도 인식)
+	 */
+	
+	
+/** 객체의 타입, 필드 정보를 출력하는 메서드
+ * @param obj : Object 또는 Object를 상속 받은 객체 참조 변수
+ * (Object는 모든 클래스/객체의 최상위 부모)
+ */
+	
+public void printObject(Object obj) {
+		
+		// obj에 전달 받을 수 있는 타입 : Object, Parent, Child
+		
+		String result = null;
+		
+		/* 자식 타입 부터 검사! */
+		if(obj instanceof Child) {
+			// obj가 참조하는 객체의 타입이
+			// Child 또는 Child를 상속 받은 자식 타입인 경우
+			
+			Child c = (Child)obj; // Child 다운 캐스팅
+			result = "[Child Type]\n";
+			result += c.getLastName() + " / " 
+			        + c.getMoney() + " / "
+			        + c.getCar();
+		}
+		
+		else if(obj instanceof Parent) {
+			// obj가 참조하는 객체의 타입이
+			// Parent 또는 Parent를 상속 받은 자식 타입인 경우
+			
+			Parent p = (Parent)obj; // Parent로 다운 캐스팅
+			result = "[Parent Type]\n";
+			result += p.getLastName() + " / " + p.getMoney();
+		}
+		
+//		else if(obj instanceof Object) { 
+		else {
+			// obj가 참조하는 객체의 타입이
+			// Object 또는 Object를 상속 받은 자식 타입인 경우
+			// -> 모든 클래스/객체는 Object의 자식이기 때문에
+			//    else에 모두 걸림
+			result = "[Object Type] 필드 없음";
+		} 
+		
+		System.out.println("-------------------------------------");
+		System.out.println(result);
+		System.out.println("-------------------------------------");
+	}
+	
+	// 매개 변수의 다형성 확인
+	public void test1() {
+		
+		Object obj    = new Object();
+		Parent parent = new Parent("김", 500);
+		Child  child  = new Child("이", 6000, "소나타");
+		
+		printObject(obj);    // 같은 Object 타입 전달 가능
+		printObject(parent); // Object의 자식 타입인 Parent 전달 가능
+		printObject(child);  // Object의 후손 타입인 Child 전달 가능
+		
+	}
+
+	/* [반환형의 다형성]
+	 * - 메서드의 반환형을 부모 타입을 설정하여
+	 *  return되는 자식 객체의 주소를 참조할 수 있게 함
+	 *  
+	 *  - 다형성의 업캐스팅 적용
+	 */
+	
+	/**
+	 * 전달 받은 num 값에 따라 알맞은 객체를 생성/반환하는 메서드
+	 * @param num : 1 - Child, 2 - Parent, 나머지 - Object
+	 * @return 생성된 객체 참조 주소 반환
+	 */
+	
+	public Object getInstance(int num) {
+		
+		// if, for, while의
+		// {} 내부 코드가 한 줄인 경우 {} 생략 가능 -> 메소드,클래스는 불가
+		if(num == 1) return new Child(); // num = 1 이면 child객체를 만들어서 반환한다
+		
+		if(num == 2) return new Parent();
+		
+		return new Object(); // if 문 둘다 안걸리면 Objcet로 객체생성하겠다
+	}
+	
+	// 반환형의 다형성 확인
+	public void test2() {
+		
+		// getInstance()의 반환형의 Object
+		
+		// o1, o2는 업캐스팅 상태
+		Object o1 = getInstance(1); // 실제로는 Child 객체 반환
+		Object o2 = getInstance(2); // Parent 객체 반환
+		Object o3 = getInstance(3); // Object 객체 반환
+		
+		// 매개변수의 다형성 + 반환형의 다형성
+		printObject(o1);
+		printObject(o2);
+		printObject(o3);
+	}
+	
+	/*[바인딩(Binding]
+	 * - 메서드 호출부와 실제 실행할 메서드 코드를 연결하는 것
+	 * 
+	 * [정적 바인딩]
+	 * - 프로그램 실행 전
+	 *  컴파일 단계에서 메서드와 메서드 호출부를 연결
+	 *  
+	 * [동적 바인딩]
+	 * - 프로그램 실행 중(Runtime)
+	 *  정적 바인딩된 메서드를
+	 *  실제 객체 타입을 기준으로 연결
+	 *  
+	 *  - 왜 사용?
+	 *  1) 다운 캐스팅의 번거로움으 없앰
+	 *  2) 재정의된 메서드(조금 더 효율적, 또는 알맞는 기능)을 수행
+	 *  
+	 *  *상속, 오버라이딩, 업캐스팅(+메모리 구조)
+	 */
+	
+	// 바인딩 확인
+	public void test3() {
+		
+		Parent p1 = new Parent("김",10); // 부모 = 부모
+		Child c1 = new Child("최", 10, "캐스퍼"); // 자식 = 자식
+		
+		Parent p2 = new Child("박", 30, "포터"); // 부모 = 자식(업캐스팅)
+		
+		// 정적 바인딩 : 프로그램 실행X, 컴파일 단계에서 연결
+		
+		//(정적)String section01.dto.Parent.toString ()
+		//(동적)String section01.dto.Parent.toString ()
+		System.out.println("p1.toString() :" + p1.toString());
+		
+		//(정적)String section01.dto.Child.toString ()
+		//(동적)String section01.dto.Child.toString ()
+		System.out.println("c1.toString() :" + c1.toString());
+		
+		//(정적)String section01.dto.Parent.toString ()
+		//(동적)String section01.dto.Child.toString ()
+		System.out.println("p2.toString() :" + p2.toString());
+		
+	}
+	
+	/* [객체 배열의 다형성]
+	 * - 부모 타입의 참조 변수를 묶음으로 다루어서
+	 * 배열 각 요소에 업캐스팅 적용
+	 * 
+	 * * 객체 배열 : 객체 참조 변수의 묶음
+	 */
+	
+	// 객체 배열의 다형성 확인
+	public void test4() {
+		
+		// Object 객체 참조 변수 5칸 짜리 배열 생성해서
+		// Object[] 참조 변수 arr에 배열 주소 대입
+		Object[] arr = new Object[5];
+		
+		for(int i=0; i <arr.length; i++) {
+			arr[i] = getInstance(i % 3 + 1); 
+			// i     : 0 1 2 3 4 
+			// i%3+1 : 1 2 3 1 2
+			
+			// 0,3번 : Child
+			// 1,4번 : Parent
+			// 2번   : Object
+		}
+		
+		System.out.println("--------------------------------------");
+	
+		// 객체 배열의 다형성 + 동적 바인딩
+		
+		// * 동적 바인딩
+		// - 실행 중 메서드 호출부 연결이 실제 참조하는 객체의 
+		// 오버라이딩된 메서드로 연결
+		// ( 오버라이딩된 메서드가 우선권을 갖는다)
+		
+		for(Object obj : arr ) { // 향상된 for문  배열중 꺼낼것 : 배열
+			
+			// 실제 참조 중인 객체의 toString()이 호출됨
+			System.out.println(obj.toString());
+			
+		}
+		
+		System.out.println("-------------------------------------");
+		System.out.println("[동적 바인딩이 없었더라면..]");
+		
+		for(Object obj : arr ) {
+			
+			// 참조 객체 타입 검사 후 다운 캐스팅
+			if(obj instanceof Child) {
+				System.out.println(((Child)obj).toString());
+			} 
+			else if(obj instanceof Parent) {
+				System.out.println(((Parent)obj).toString());
+			}
+			else {
+				System.out.println(obj.toString());
+			}
+		
+		}
+		
+		
+		/* debug 모드
+		 * - 이클립스 등 IDE에서 제공하는 기능
+		 * - 코드를 수행 중 지정된 지점(break point)에서
+		 *  실행을 멈추고 
+		 *  해당 시점에 조재하는 변수/필드 값을 확인하는 모드
+		 */
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}	
